ca1b60e4e022d2d03706a15e92e639dcfccee54a
quietshu <ds303077135@gmail.com>
2015-10-10 16:05:56 +0800

    Users & quizzes added

--- routes/admin.js
+++ routes/admin.js
@@ -1,39 +1,18 @@
 var express = require('express');
 var router  = express.Router();
 
-var config = require('../config.json');
+var status = require('./admin/status');
+var users = require('./admin/users');
+var quizzes = require('./admin/quizzes');
+var settings = require('./admin/settings');
 
-/* GET users listing. */
-router.get('/', function (req, res, next) {
-    res.render('admin', {
-        title: config.title,
-        user:  req.user,
-        page:  'status'
-    });
-});
+router.get('/', status);
+router.get('/users', users.pages);
+router.get('/quizzes', quizzes.pages);
+router.get('/quizzes/new', quizzes.getNew);
+router.get('/quizzes/:id', quizzes.id);
+router.get('/settings', settings);
 
-router.get('/users', function (req, res, next) {
-    res.render('admin', {
-        title: config.title,
-        user:  req.user,
-        page:  'users'
-    });
-});
-
-router.get('/quizzes', function (req, res, next) {
-    res.render('admin', {
-        title: config.title,
-        user:  req.user,
-        page:  'quizzes'
-    });
-});
-
-router.get('/settings', function (req, res, next) {
-    res.render('admin', {
-        title: config.title,
-        user:  req.user,
-        page:  'settings'
-    });
-});
+router.post('/quizzes/new', quizzes.postNew);
 
 module.exports = router;
